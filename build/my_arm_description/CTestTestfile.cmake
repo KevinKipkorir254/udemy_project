ac0780e5a45731cacbcd7a45019ae69e21a17f99
KevinKipkorir254 <kipkorir.magut254@gmail.com>
2023-10-20 17:01:20 +0300

    for safety

---
+++ build/my_arm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kevo/ros_ws/src/udemy_project/src/my_arm_description
+# Build directory: /home/kevo/ros_ws/src/udemy_project/build/my_arm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
